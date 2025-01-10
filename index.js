dbf8b5752d81646e49af92985ce49e5c4806b00e
unknown <matt.lu@mail.utoronto.ca>
2025-01-09 19:35:29 -0500

    add signup

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const postgres = require('postgres');
+const sql = postgres('postgresql://allears-db_owner:JkCDGHu6FU8m@ep-flat-butterfly-a50qpai2-pooler.us-east-2.aws.neon.tech/allears-db?sslmode=require');
 app.use(express.json());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.listen(PORT, () =>{
     console.log("app running on " + PORT)
@@ -17,12 +27,25 @@ app.get('/', (req,res) => {
 //Signup endpoint
 app.post('/signup', async (req, res) => {
     const username = req.body.username;
-    const salt = bcrypt.genSaltSync(10);
-    const passwordHash = await bcrypt.hash(req.body.password, salt);
-    res.status(200).send({
-        receivedUsername: username,
-        passwordHash: passwordHash
-    })
+    const passwordHash = await bcrypt.hash(req.body.password, bcrypt.genSaltSync(10));
+    try{
+        const query = await sql`
+        insert into users (
+          username, password
+        ) values (
+          ${username}, ${passwordHash}
+        )
+      `
+      res.status(200).send({
+        message: 'Successfully signed up ' + username
+      })
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send({
+            message: "We couldn't sign you up. Please check if this username is already registered."
+        })
+    }
 })
 
 //Login endpoint
